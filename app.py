d5212533a74d1cd46fb881a2baa542ba85f05fd9
Rkrohit371 <rohitkg1998@gmail.com>
2020-05-09 18:03:01 +0530

    Added required files fixed

--- app.py
+++ app.py
@@ -16,9 +16,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'rohit'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt = JWT(app, authenticate, identity)
 
